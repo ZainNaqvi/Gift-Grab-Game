7f693c271abc66658971b2d8d987f1801ea7f49d
Zain Naqvi <Zain.N@devnatives.com>
2023-05-23 18:43:00 +0500

    added gamewidget

---
+++ lib/game/gift_grab_game.dart
@@ -0,0 +1,8 @@
+import 'package:flame/game.dart';
+
+class GiftGrabGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'game/gift_grab_game.dart';
+
+void main() {
+  runApp(GameWidget(game: GiftGrabGame()));
+}
